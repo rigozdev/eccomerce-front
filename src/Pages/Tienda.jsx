88badb889ea5e17594600da7df23c6f054a929bb
rigozdev <rodrigo.gonzaleztr@outlook.com>
2024-02-20 10:02:00 -0300

    chore: Material UI modules. feat: Component Hero | Pages/Tienda

---
+++ src/Pages/Tienda.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero/Hero";
+
+function Tienda() {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+}
+
+export default Tienda;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import "./index.css";
+
+import { CssBaseline } from "@mui/material";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
